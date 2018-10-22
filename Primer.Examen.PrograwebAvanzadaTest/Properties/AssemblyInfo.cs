16d8de805e4c02207640c3342a2a568f496b8158
mpadillac <m4yronpadilla@gmail.com>
2018-10-22 11:28:51 -0600

    primerexamen

---
+++ Primer.Examen.PrograwebAvanzadaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Primer.Examen.PrograwebAvanzadaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Primer.Examen.PrograwebAvanzadaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19b3428b-aaa6-4224-9e67-2c496242b7a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
